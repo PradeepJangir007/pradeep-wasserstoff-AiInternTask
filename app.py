bac5090dd6da8573003d8c64a8f903afdc3acb2d
PradeepJangir077 <pradeepjangir.ml007@gmail.com>
2024-10-15 17:06:27 +0530

    fix error

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import logging
 from collections import Counter
 import re
 import time
+import torch
 import nltk
 from nltk.corpus import stopwords
 from transformers import T5ForConditionalGeneration, T5Tokenizer
@@ -23,9 +24,12 @@ path = st.text_input("folder path")
 b=st.button('generate a summary')
 if b==True:
     if __name__ == "__main__":
-        start= time.time()
-        ts=mymodule
-        d=ts.main(path)
-        end= time.time()
-        st.write(f"PDF processing completed.\n In {round(end-start)} seconds")
+        try :
+            start= time.time()
+            ts=mymodule
+            d=ts.main(path)
+            end= time.time()
+            st.write(f"PDF processing completed.\n In {round(end-start)} seconds")
+        except Exception as e:
+        logging.error(f"Error : {str(e)}")
         
